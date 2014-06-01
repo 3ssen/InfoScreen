dd2fb887d395e83b1ec510e3e7784bb7ccf22e8d
3ssen <rick3ssen@gmail.com>
2014-06-01 11:20:09 +0200

    Update calendar.php

--- calendar.php
+++ calendar.php
@@ -1,3 +1,4 @@
 <?php
 	$url = 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M';
+	$url = 'https://www.google.com/calendar/ical/rick3ssen%40gmail.com/private-33d5be34c66ce8e3ea452ae11e3acae6/basic.ics';
 	echo file_get_contents($url);
